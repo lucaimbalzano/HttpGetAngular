2e461457c110698b21cbe7227c16c31619efa9d5
luca <lucaimbalzano@gmail.com>
2021-07-19 19:29:21 +0200

    ##Routing Architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'table', component: TableComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/dashboard' , pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
